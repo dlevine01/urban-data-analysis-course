548ca7b255c4363b034e183955eb875cdcea15af
dlevine01 <dlevine01@gmail.com>
2025-04-17 13:51:36 -0400

    post notebook and add geopandas intro

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 # Packages to install
-$packages = @("ipykernel","pandas","seaborn","matplotlib","openpyxl","scipy","xlrd")
+$packages = @("ipykernel","pandas","seaborn","matplotlib","openpyxl","scipy","xlrd", "geopandas[all]", "requests")
 
 # Set exception policy
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
